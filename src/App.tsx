108a381f22e0ad589250a237974671282bc9b131
Akshat Apoorv <akshatgamer120@gmail.com>
2025-03-05 10:47:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/team" element={<Testimonials />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/whysupport" element={<WhySupport />} />
         </Routes>
         <Footer />
       </div>
